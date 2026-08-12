9447279b925623cd4ea6632e78e19da2bd49e31e
Berline Britus <britusberline46@gmail.com>
2026-08-12 04:52:51 +0000

    Deploy static site to gh-pages

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import createMiddleware from "next-intl/middleware";
-import { routing } from "./routing";
-
-export default createMiddleware(routing);
-
-export const config = {
-  // Match all pathnames except for API routes, static files, and Next internals.
-  matcher: ["/((?!api|_next|_vercel|.*\\..*).*)"],
-};
